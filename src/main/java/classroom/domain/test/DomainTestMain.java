8d638df935f9195d2cd7264d86181eaa5968c32b
github_MostafaAttaSayed <mostafa.atta2015@gmail.com>
2019-11-26 23:32:08 +0200

    Common Repo Impl edit method #3

--- src/main/java/classroom/domain/test/DomainTestMain.java
+++ src/main/java/classroom/domain/test/DomainTestMain.java
@@ -3,6 +3,7 @@ package classroom.domain.test;
 import classroom.dal.entities.BaseEntity;
 import classroom.dal.entities.Embeddable.CompositeName;
 import classroom.dal.entities.Student;
+import classroom.dal.hibernate.HibernateDBManager;
 import classroom.dal.primitives.Gender;
 
 import java.math.BigDecimal;
@@ -13,10 +14,9 @@ public class DomainTestMain
 	{
 		try
 		{
-			Student student = addStudent();
-			BusinessLogicCore blCore = new BusinessLogicCore();
-			BaseEntity addedStudent = blCore.add(student);
-			System.out.println("New student added with id : " + addedStudent.getId());
+			//			addStudent();
+			editStudent();
+
 		}
 		catch (Exception e)
 		{
@@ -24,16 +24,48 @@ public class DomainTestMain
 		}
 	}
 
-	private static Student addStudent()
+	private static void editStudent()
 	{
-		Student student = new Student();
-		student.setEmail("ahmed.atta@gmail");
-		student.setGender(Gender.Male);
-		student.setGpa(new BigDecimal(3.0));
-		CompositeName name = new CompositeName();
-		name.setFirstName("Ahmed");
-		name.setLastName("Ali");
-		student.setName(name);
-		return student;
+		try
+		{
+			BusinessLogicCore blCore = new BusinessLogicCore();
+			Student student = new Student();
+			Student foundEntity = blCore.find(student, (long) 1);
+			foundEntity.setEmail("ahmed@yahoo.com");
+			BaseEntity editStudent = blCore.edit(foundEntity);
+			showEntityResult(editStudent, "Edit");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void showEntityResult(BaseEntity entity, String type)
+	{
+		System.out.println(type + "   >>     id : " + entity.getId());
+	}
+
+	private static void addStudent()
+	{
+		try
+		{
+			BusinessLogicCore blCore = new BusinessLogicCore();
+			Student student = new Student();
+			student.setEmail("ahmed.atta@gmail");
+			student.setGender(Gender.Male);
+			student.setGpa(new BigDecimal(3.0));
+			CompositeName name = new CompositeName();
+			name.setFirstName("Ahmed");
+			name.setLastName("Ali");
+			student.setName(name);
+			BaseEntity addedStudent = blCore.add(student);
+			showEntityResult(addedStudent, " Add Entity");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			HibernateDBManager.rollbackTransaction();
+		}
 	}
 }
