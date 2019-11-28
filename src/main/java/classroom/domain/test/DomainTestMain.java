e6c716a95f36d12a6b911f48af1c9dee85d83a8c
AhmedSabry <ahmedsabry1894@gmail.com>
2019-11-28 10:26:41 +0200

    edit in main method

--- src/main/java/classroom/domain/test/DomainTestMain.java
+++ src/main/java/classroom/domain/test/DomainTestMain.java
@@ -1,8 +1,7 @@
 package classroom.domain.test;
 
-import classroom.dal.entities.BaseEntity;
+import classroom.dal.entities.*;
 import classroom.dal.entities.Embeddable.CompositeName;
-import classroom.dal.entities.Student;
 import classroom.dal.hibernate.HibernateDBManager;
 import classroom.dal.primitives.Gender;
 
@@ -14,9 +13,42 @@ public class DomainTestMain
 	{
 		try
 		{
-			//			addStudent();
-			editStudent();
+			addStudent();
+			addInstructor();
+			addCourse();
+			addUser();
+			//			editStudent();
+			//			findStudent();
+			//			deleteStudent();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 
+	private static void deleteStudent()
+	{
+		try
+		{
+			BusinessLogicCore core = new BusinessLogicCore();
+			Student foundStudent = core.find(Student.class, (long) 2);
+			core.delete(foundStudent);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void findStudent()
+	{
+		try
+		{
+			BusinessLogicCore core = new BusinessLogicCore();
+			Student foundStudent = core.find(Student.class, (long) 1);
+			showEntityResult(foundStudent, "Find");
+			System.out.println("first name : " + foundStudent.getName().getFirstName());
 		}
 		catch (Exception e)
 		{
@@ -29,8 +61,7 @@ public class DomainTestMain
 		try
 		{
 			BusinessLogicCore blCore = new BusinessLogicCore();
-			Student student = new Student();
-			Student foundEntity = blCore.find(student, (long) 1);
+			Student foundEntity = blCore.find(Student.class, (long) 1);
 			foundEntity.setEmail("ahmed@yahoo.com");
 			BaseEntity editStudent = blCore.edit(foundEntity);
 			showEntityResult(editStudent, "Edit");
@@ -52,15 +83,78 @@ public class DomainTestMain
 		{
 			BusinessLogicCore blCore = new BusinessLogicCore();
 			Student student = new Student();
-			student.setEmail("ahmed.atta@gmail");
-			student.setGender(Gender.Male);
-			student.setGpa(new BigDecimal(3.0));
 			CompositeName name = new CompositeName();
 			name.setFirstName("Ahmed");
 			name.setLastName("Ali");
 			student.setName(name);
+			student.setEmail("ahmed.ali@gmail.com");
+			student.setGender(Gender.Male);
+			student.setGpa(new BigDecimal(3.0));
 			BaseEntity addedStudent = blCore.add(student);
 			showEntityResult(addedStudent, " Add Entity");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			HibernateDBManager.rollbackTransaction();
+		}
+	}
+
+	private static void addCourse()
+	{
+		try
+		{
+			BusinessLogicCore blCore = new BusinessLogicCore();
+			Course course = new Course();
+			course.setName("Data Structure");
+			course.setCode("002");
+			course.setPreRequest(true);
+			Instructor instructor = new Instructor();
+			instructor.setId((long) 1);
+			course.setInstructor(instructor);
+			BaseEntity addedCourse = blCore.add(course);
+			showEntityResult(addedCourse, " Add Entity");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			HibernateDBManager.rollbackTransaction();
+		}
+	}
+
+	private static void addInstructor()
+	{
+		try
+		{
+			BusinessLogicCore blCore = new BusinessLogicCore();
+			Instructor instructor = new Instructor();
+			instructor.setName("dr.Ahmed");
+			instructor.setEmail("dr.ahmed@gmail.com");
+			instructor.setAddress("Dokki");
+			instructor.setPhone("01065102524");
+			BaseEntity addedInstructor = blCore.add(instructor);
+			showEntityResult(addedInstructor, " Add Entity");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			HibernateDBManager.rollbackTransaction();
+		}
+	}
+
+	private static void addUser()
+	{
+		try
+		{
+			BusinessLogicCore blCore = new BusinessLogicCore();
+			User user = new User();
+			CompositeName compositeName = new CompositeName();
+			compositeName.setFirstName("mohamed");
+			compositeName.setLastName("mahmoud");
+			user.setName(compositeName);
+			user.setEmail("moamed_mahmoud20@gmail.com");
+			BaseEntity addedUser = blCore.add(user);
+			showEntityResult(addedUser, " Add Entity");
 		}
 		catch (Exception e)
 		{
