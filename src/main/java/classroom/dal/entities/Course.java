b3b658886260f46c90148606446623f771fa5532
github_MostafaAttaSayed <mostafa.atta2015@gmail.com>
2019-12-08 21:25:58 +0200

    add hours to course entity

--- src/main/java/classroom/dal/entities/Course.java
+++ src/main/java/classroom/dal/entities/Course.java
@@ -3,16 +3,28 @@ package classroom.dal.entities;
 import classroom.dal.roots.*;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Course extends BaseEntity
 {
+	BigDecimal numOfHours;
 	private Boolean isPreRequest;
 
 	public Course()
 	{
 
+	}
+
+	public BigDecimal getNumOfHours()
+	{
+		return numOfHours;
+	}
+
+	public void setNumOfHours(BigDecimal numOfHours)
+	{
+		this.numOfHours = numOfHours;
 	}
 
 	public Boolean getPreRequest()
