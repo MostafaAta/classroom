a1283b389c5b3f0c371c9ef07d107345cf302df0
github_MostafaAttaSayed <mostafa.atta2015@gmail.com>
2019-12-11 22:28:37 +0200

    edit course rating relation details

--- src/main/java/classroom/dal/entities/CourseRating.java
+++ src/main/java/classroom/dal/entities/CourseRating.java
@@ -20,7 +20,7 @@ public class CourseRating extends BaseEntity
 	private BigDecimal rating;
 	@Fetch(FetchMode.JOIN)
 	@JoinColumn(name = "courseRating_id")
-	@OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
+	@OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
 	private List<CourseRatingLine> details;
 
 	public CourseRating()
